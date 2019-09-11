e24bffbdbb67ede0001ec61dcf58c72597a455d9
muthangashem <muthangashem@gmail.com>
2019-09-11 21:12:16 +0300

    add router-store and create customserializer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
+import { reducers, CustomSerializer } from './store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlogModule } from './blog/blog.module';
+import { StoreRouterConnectingModule, RouterStateSerializer } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [
@@ -18,18 +19,20 @@ import { BlogModule } from './blog/blog.module';
     BrowserModule,
     AppRoutingModule,
     BlogModule,
-    StoreModule.forRoot({}, {
+    StoreModule.forRoot(reducers, {
       runtimeChecks: {
         strictStateImmutability: true,
         strictActionImmutability: true
       }
     }),
+    StoreRouterConnectingModule,
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({
       maxAge: 10
-    })
+    }),
+    StoreRouterConnectingModule.forRoot()
   ],
-  providers: [],
+  providers: [{ provide: RouterStateSerializer, useClass: CustomSerializer }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
