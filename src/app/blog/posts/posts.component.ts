5ef260fce23004861a83b15f3dc36e71d3ec403f
muthangashem <muthangashem@gmail.com>
2019-09-11 19:31:27 +0300

    create posts interface and posts component

---
+++ src/app/blog/posts/posts.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Store } from '@ngrx/store';
+import * as fromStore from '../store'
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.scss']
+})
+export class PostsComponent implements OnInit {
+
+  constructor(private store: Store<fromStore.BlogState>) { }
+
+  ngOnInit() {
+    this.store.select('blog').subscribe(state => {
+      console.log(state);
+    })
+  }
+
+}
